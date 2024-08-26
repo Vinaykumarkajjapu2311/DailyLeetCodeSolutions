b4f897d639e7c5e8d46da6c990af18035e0c1e14
KAJJAPU VINAY KUMAR <150517822+Vinaykumarkajjapu2311@users.noreply.github.com>
2024-08-26 14:47:15 +0530

    Time: 67 ms (91.29%), Space: 70.6 MB (19.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         int curSum=0;
         int maxi=nums[0];
+        if(n==1)return nums[0];
         for(int i=0;i<n;i++)
         {
             if(curSum<0)curSum=0;
