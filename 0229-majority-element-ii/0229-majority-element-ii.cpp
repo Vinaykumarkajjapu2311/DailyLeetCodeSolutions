0a962d49da5697dde2407a98f641ea68405adc8e
KAJJAPU VINAY KUMAR <150517822+Vinaykumarkajjapu2311@users.noreply.github.com>
2024-08-27 23:23:12 +0530

    Time: 10 ms (48.27%), Space: 18.9 MB (34.83%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mpp;
+        vector<int>ans;
+        int mini=(int)(n/3)+1;
+        for(int i=0;i<n;i++)
+        {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==mini)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+        
+    }
+};
