5627c78c766fa7f1012babb95109ecc566004df2
KAJJAPU VINAY KUMAR <150517822+Vinaykumarkajjapu2311@users.noreply.github.com>
2024-09-11 09:23:14 +0530

    Time: 2 ms (87.84%), Space: 11.6 MB (59.52%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>& arr,int n,vector<vector<int>>&dp)
-    {
-        if(i==n-1)return arr[n-1][j];
-        if(dp[i][j]!=-1)return dp[i][j];
+    
+    int minimumTotal(vector<vector<int>>& arr) {
+        int n=arr.size();
+        vector<int>next(n,-1);
+        for(int i=n-1;i>=0;i--)
+        {
+            next[i]=arr[n-1][i];
+        }
 
-        int down=arr[i][j]+f(i+1,j,arr,n,dp);
-        int dig=arr[i][j]+f(i+1,j+1,arr,n,dp);
-        return dp[i][j]=min(down,dig);
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans=f(0,0,triangle,n,dp);
-        return ans;
+        for(int i=n-2;i>=0;i--)
+        {
+            vector<int>cur(n,-1);
+            for(int j=i;j>=0;j--)
+            {
+                int down=arr[i][j]+next[j];
+                int diag=arr[i][j]+next[j+1];
+                cur[j]=min(diag,down);
+            }
+            next=cur;
+        }
+        return next[0];
     }
 };
