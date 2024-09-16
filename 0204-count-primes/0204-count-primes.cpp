0873524afdd73f341951eff5b80fd5080615c317
KAJJAPU VINAY KUMAR <150517822+Vinaykumarkajjapu2311@users.noreply.github.com>
2024-09-16 22:19:47 +0530

    Time: 108 ms (95.42%), Space: 131.8 MB (20.48%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;  // No primes less than 2
+        if (n < 2) return 0;  // No primes less than 2
 
-        vector<int> prime(n, 1);  // Create a vector of size n and initialize all to 1 (true)
-        prime[0] = prime[1] = 0;  // 0 and 1 are not prime numbers
+        vector<int> prime(n, 1); 
+        prime[0] = prime[1] = 0; 
 
         for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
-                // Mark multiples of i as non-prime
+               
                 for (int j = i * i; j < n; j += i) {
                     prime[j] = 0;
                 }
             }
         }
-
-        // Count the number of primes less than n
         int cnt = 0;
         for (int i = 2; i < n; i++) {
             if (prime[i]) cnt++;
